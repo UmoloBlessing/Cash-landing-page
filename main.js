6af6dcb72b8890b7126ebc225b31ea4953cca995
Umolo Blessing Johnson <umoloblessing009@gmail.com>
2023-08-07 02:03:26 +0100

    update

--- main.js
+++ main.js
@@ -5,31 +5,28 @@ const quoteButtons = document.querySelectorAll('.quote');
 const quoteSection = document.getElementById('quote-1');
 const quoteImage = document.getElementById('img-1');
 
-
-console.log(quoteButtons)
-
 menuButton.addEventListener('click', () => {
-    menuSection.classList.add('visible'); 
-    document.body.classList.add('menu-open'); 
+    menuSection.classList.toggle('open');
+    document.body.classList.toggle('menu-open');
 });
 
-  menuImage.addEventListener('click', () => {
-    menuSection.classList.remove('visible'); 
-    document.body.classList.remove('menu-open'); 
+menuImage.addEventListener('click', () => {
+    menuSection.classList.toggle('open');
+    document.body.classList.toggle('menu-open');
 });
-
 
 quoteButtons.forEach(button => {
     button.addEventListener('click', () => {
-        quoteSection.classList.add('visible'); 
-        document.body.classList.add('quote-open'); 
+        quoteSection.classList.toggle('open');
+        document.body.classList.toggle('quote-open');
     });
 });
 
 quoteImage.addEventListener('click', () => {
-    quoteSection.classList.remove('visible'); 
-    document.body.classList.remove('quote-open'); 
+    quoteSection.classList.toggle('open');
+    document.body.classList.toggle('quote-open');
 });
+
 
 
 
